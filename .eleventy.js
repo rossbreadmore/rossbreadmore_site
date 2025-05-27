5a9d8b7c4824b604ff590db3cf1036be1135773a
Ross Breadmore <rossbreadmore@Rosss-MacBook-Air.local>
2025-05-27 18:43:42 +0100

    sort out blog issuee

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./posts/*.md").reverse();
+  });
+
   return {
     dir: {
       input: ".",
---
+++ posts/where-it-beings.md
@@ -0,0 +1,7 @@
+---
+title: Where it begins
+date: 2025-05-05
+layout: post.njk
+---
+
+Describing how I'm using AI
