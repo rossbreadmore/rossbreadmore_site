0eae5b3bf1611eae4454d4e7e31c6d54e3ae521b
Ross Breadmore <rossbreadmore@Rosss-MacBook-Air.local>
2025-06-02 20:59:39 +0100

    search removal

--- .eleventy.js
+++ .eleventy.js
@@ -21,3 +21,10 @@ module.exports = function (eleventyConfig) {
     }
   };
 };
+eleventyConfig.addCollection("tagList", collection => {
+  const tags = new Set();
+  collection.getAll().forEach(item => {
+    (item.data.tags || []).forEach(tag => tags.add(tag));
+  });
+  return [...tags];
+});
